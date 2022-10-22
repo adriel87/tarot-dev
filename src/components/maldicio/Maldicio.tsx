4e0075efa3c522708b503f6393134efd425a90c2
adriel <adrielarochaoronoz@gmail.com>
2022-10-22 21:59:27 +0100

    :memo: new form added for tarot card

--- src/components/maldicio/Maldicio.tsx
+++ src/components/maldicio/Maldicio.tsx
@@ -4,25 +4,44 @@ import { Camera, CameraResultType, Photo } from '@capacitor/camera'
 import './maldicio.css';
 import { useState } from 'react';
 import { image } from 'ionicons/icons';
-import { tarotCard as TC } from '../../interfaces';
+import { tarotCard } from '../../interfaces';
 
 
-const tarotCard : TC={
-    name:'la carta',
-    description:'es de noche',
-    invertedDescription:'es de dia',
-    url:'',
+
+
+interface formTarotCard{
+    name:'',
+    description:'',
+    invertedDescription:'',
     deploy:{
-        normal:'nada',
-        inverted:'nada1'
+        normal:'',
+        inverted:''
     },
     love:{
-        normal:'nada',
-        inverted:'nada1'
+        normal:'',
+        inverted:''
     },
     luck:{
-        normal:'nada',
-        inverted:'nada1'
+        normal:'',
+        inverted:''
+    },
+}
+
+const initState : formTarotCard={
+    name:'',
+    description:'',
+    invertedDescription:'',
+    deploy:{
+        normal:'',
+        inverted:''
+    },
+    love:{
+        normal:'',
+        inverted:''
+    },
+    luck:{
+        normal:'',
+        inverted:''
     },
     
 } 
@@ -30,6 +49,7 @@ const tarotCard : TC={
 const Maldicio : React.FC = () => {
 
     const [cameraPhoto, setCameraPhoto] = useState<Photo>()
+    const [tarotForm, setTarotForm] = useState<formTarotCard>(initState)
 
     const takePicture =async () => {
         try {
@@ -47,12 +67,105 @@ const Maldicio : React.FC = () => {
 
     }
 
+    const handleChange = (e: any) : void =>{
+        e.preventDefault()
+        const { id, value } = e.target 
+        switch (id) {
+            case 'name':
+                setTarotForm({
+                    ...tarotForm,
+                    [id]:value
+                })
+                break;
+
+            case 'description':
+            setTarotForm({
+                ...tarotForm,
+                [id]:value
+            })
+                break;
+
+            case 'invertedDescription':
+            setTarotForm({
+                ...tarotForm,
+                [id]:value
+            })
+                break;
+
+            case 'love':
+                setTarotForm({
+                    ...tarotForm,
+                    love:{
+                        ...tarotForm.love,
+                        normal:value,
+                    }
+                })
+                break;
+
+            case 'ilove':
+                setTarotForm({
+                    ...tarotForm,
+                    love:{
+                        ...tarotForm.love,
+                        inverted:value,
+                    }
+                })
+                break;
+
+            case 'luck':
+                setTarotForm({
+                    ...tarotForm,
+                    luck:{
+                        ...tarotForm.love,
+                        normal:value,
+                    }
+                })
+                break;
+
+            case 'iluck':
+                setTarotForm({
+                    ...tarotForm,
+                    luck:{
+                        ...tarotForm.love,
+                        inverted:value,
+                    }
+                })
+                break;
+
+            case 'deploy':
+                setTarotForm({
+                    ...tarotForm,
+                    deploy:{
+                        ...tarotForm.love,
+                        normal:value,
+                    }
+                })
+                break;
+
+            case 'ideploy':
+                setTarotForm({
+                    ...tarotForm,
+                    deploy:{
+                        ...tarotForm.love,
+                        inverted:value,
+                    }
+                })
+                break;
+
+            default:
+                break;
+
+        }
+        
+    }
     
+    const handleSendNewCard = async () =>{
+        // TODO send new card to back
 
-
+        // 
+    }
   return (
   <>
-  
         <IonCard>
             <IonCardHeader>
                 <IonCardTitle className='title-maldicio'>Crea tu Carta</IonCardTitle>
@@ -66,21 +179,105 @@ const Maldicio : React.FC = () => {
 
     
             <IonList lines="none">
-                <IonItem className='item-maldicio'>
-                    <IonInput className='input-maldicio' color="primary" placeholder="El nombre de la carta"></IonInput>
+                
+                <IonItem color='vayaShit' className='item-maldicio' button>
+                    <IonLabel className='label-maldicio' position='floating' color='primary'>
+                        <span className='label-maldicio'>name</span>
+                    </IonLabel>
+                    <IonInput 
+                        id='name' 
+                        onIonChange={(e)=>handleChange(e)} 
+                        className='input-maldicio' 
+                        color="primary"
+                        ></IonInput>
                 </IonItem>
-                <IonItem>
-                    <IonInput color="success" placeholder="descripcion"></IonInput>
+                <IonItem color='vayaShit'>
+                    <IonLabel className='label-maldicio' position='floating' color='success'>
+                        <span className='label-maldicio '>description</span>
+                    </IonLabel>
+                    <IonInput 
+                        id='description'
+                        onIonChange={(e)=>handleChange(e)} 
+                        color="success"
+                        ></IonInput>
                 </IonItem>
-                <IonItem className='item-maldicio'>
-                    <IonInput color="primary" placeholder="y su invertida"></IonInput>
+                <IonItem color='vayaShit' className='item-maldicio'>
+                    <IonLabel className='label-maldicio' position='floating' color='primary'>
+                        <span className='label-maldicio'>inverted description</span>
+                    </IonLabel>
+                    <IonInput 
+                        id='invertedDescription'
+                        onIonChange={(e)=>handleChange(e)} 
+                        className='input-maldicio' 
+                        color="primary" 
+                        value={tarotForm.invertedDescription}
+                        ></IonInput>
+                </IonItem>
+                <IonItem color='vayaShit'>
+                    <IonLabel className='label-maldicio' position='floating' color='success'>
+                        <span className='label-maldicio'>love</span>
+                    </IonLabel>
+                    <IonInput
+                        id='love' 
+                        onIonChange={(e)=>handleChange(e)} 
+                        color="success" 
+                        ></IonInput>
+                </IonItem>
+                <IonItem color='vayaShit' className='item-maldicio'>
+                    <IonLabel className='label-maldicio' position='floating' color='primary'>
+                        <span className='label-maldicio'>inverted love</span>
+                    </IonLabel>
+                    <IonInput
+                        id='ilove' 
+                        onIonChange={(e)=>handleChange(e)} 
+                        className='input-maldicio' 
+                        color="primary" 
+                        ></IonInput>
+                </IonItem>
+                <IonItem color='vayaShit'>
+                    <IonLabel className='label-maldicio' position='floating' color='success'>
+                        <span className='label-maldicio'>luck</span>
+                    </IonLabel>
+                    <IonInput
+                        id='luck' 
+                        onIonChange={(e)=>handleChange(e)} 
+                        color="success" 
+                        ></IonInput>
+                </IonItem>
+                <IonItem color='vayaShit' className='item-maldicio'>
+                    <IonLabel className='label-maldicio' position='floating' color='primary'>
+                        <span className='label-maldicio'>inverted luck</span>
+                    </IonLabel>
+                    <IonInput
+                        id='iluck' 
+                        onIonChange={(e)=>handleChange(e)} 
+                        className='input-maldicio' 
+                        color="primary" 
+                        ></IonInput>
+                </IonItem>
+                <IonItem color='vayaShit'>
+                    <IonLabel className='label-maldicio' position='floating' color='success'>
+                        <span className='label-maldicio'>deploy</span>
+                    </IonLabel>
+                    <IonInput 
+                        id='deploy'
+                        onIonChange={(e)=>handleChange(e)} 
+                        color="success" 
+                        ></IonInput>
+                </IonItem>
+                <IonItem color='vayaShit' className='item-maldicio'>
+                    <IonLabel className='label-maldicio' position='floating' color='primary'>
+                        <span className='label-maldicio'>inverted deploy</span>
+                    </IonLabel>
+                    <IonInput
+                        id='ideploy' 
+                        onIonChange={(e)=>handleChange(e)} 
+                        className='input-maldicio' 
+                        color="primary" 
+                        ></IonInput>
                 </IonItem>
             </IonList>
-
-
         </IonCard>
-
-
   </>
   );
 };
